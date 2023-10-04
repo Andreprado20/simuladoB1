96f4c854e8d5cbd35ed4b5c08fa57da2314bef91
André Prado <andrepradooliveira16@gmail.com>
2023-10-04 11:35:23 -0300

    creating server

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,22 @@
-import express from 'express'
-import * as dotenv from 'dotenv'
+import express from "express";
 
+import * as dotenv from "dotenv";
+import PatientRouter from "./routes/PatientRoutes";
+
+dotenv.config({ path: process.env.NODE_ENV === "dev" ? ".env.dev" : ".env" });
+
+const app = express();
+
+app.use("/api", PatientRouter);
+// app.use("/api", ConsultaRouter);
+// app.use("/api", AgendaRouter);
+// app.use("/api", SecretariaRouter);
+
+
+if (process.env.PORT) {
+  app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+  });
+} else {
+  console.log("Fail to load environment");
+}
