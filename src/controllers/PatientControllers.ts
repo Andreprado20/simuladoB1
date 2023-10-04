cb13d90abfe35f0d93e0498418d5cc55c38ecfca
André Prado <andrepradooliveira16@gmail.com>
2023-10-04 10:36:01 -0300

    adding code to the PatientCOntrollers.ts file

--- src/controllers/PatientControllers.ts
+++ src/controllers/PatientControllers.ts
@@ -1,5 +1,58 @@
-import { PrismaClient } from '@prisma/client'
+import PatientServices from '../services/PatientServices'
 import { Request , Response } from 'express'
 
-const prisma = new PrismaClient()
+class PatientController{
+    constructor(){}
+
+    async listarPacientes(req: Request, res: Response){
+        const pacientes = await PatientServices.listarPacientes();
+        return res.status(200).json({
+            status: "ok",
+            pacientes: pacientes
+        })
+    }
+
+    async criarPaciente(req: Request, res: Response, nome: string, senha: number, usuario: string){
+        const criaPaciente = await PatientServices.criarPaciente(nome, senha, usuario);
+        return res.status(200).json(
+            {
+                status: "ok",
+                mensagem: `Paciente ${criaPaciente} cadastrado!`
+            }
+        )
+    }
+
+    async updatePaciente(req: Request, res: Response, id: number, novoNome: string, novaSenha: number, novoUsuario: string){
+        const atualizaPaciente = await PatientServices.updatePaciente(id, novoNome, novaSenha, novoUsuario);
+        return res.status(200).json(
+            {
+                status: "ok",
+                mensagem: `Paciente ${atualizaPaciente} alterado!`
+            }
+        )
+    }
+
+    async deletePaciente(req: Request, res: Response, id: number){
+        const deletaPaciente = await PatientServices.deletePaciente(id);
+        return res.status(200).json(
+            {
+                status: "ok",
+                mensagem: `Paciente ${deletaPaciente} deletado!`
+            }
+        )
+    }
+
+    async buscaPaciente(req: Request, res: Response, id: number){
+        const buscarPaciente = await PatientServices.buscaPaciente(id);
+        return res.status(200).json(
+            {
+                status: "ok",
+                paciente: buscarPaciente
+            }
+        )
+    }
+}
+
+export default new PatientController()
+
 
--- src/services/PatientServices.ts
+++ src/services/PatientServices.ts
@@ -63,6 +63,7 @@ class PatientServices{
                     }
                 }
             )
+            return `Paciente ${deletarPaciente.nome} deletado!`
         } catch (error){
             console.log(error)
         }
@@ -77,6 +78,7 @@ class PatientServices{
                    } 
                 }
             )
+            return buscarPaciente
         } catch (error){
             console.log(error)
         }
