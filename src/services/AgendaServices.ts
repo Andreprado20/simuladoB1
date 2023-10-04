5572f5c1b4fbb15536fd24429ca0874e49d47319
André Prado <andrepradooliveira16@gmail.com>
2023-10-04 11:26:13 -0300

    adding agenda services code

--- src/services/AgendaServices.ts
+++ src/services/AgendaServices.ts
@@ -7,23 +7,43 @@ class AgendaServices{
 
     async listarAgenda(){
         try{
-
+            const listaAgendas = await prisma.agenda.findMany()
+            return listaAgendas
         } catch(error){
             console.log(error)
         }
     }
 
-    async criarAgenda(){
+    async criarAgenda(data: Date, nomePcnt: string){
         try{
-
+            const criaAgenda= await prisma.agenda.create(
+                {
+                    data:{
+                        data,
+                        nomePcnt,
+                    }
+                }
+            )
+            return criaAgenda;
         } catch(error){
             console.log(error)
         }
     }
 
-    async updateAgenda(){
+    async updateAgenda(id: number, data: Date, nomePcnt: string){
         try{
-
+            const atualizaAgenda = await prisma.agenda.update(
+                {
+                    where:{
+                        id,
+                    },
+                    data:{
+                        nomePcnt: nomePcnt,
+                        data: data
+                    }
+                }
+            )
+            return atualizaAgenda;
         } catch(error){
             console.log(error)
         }
